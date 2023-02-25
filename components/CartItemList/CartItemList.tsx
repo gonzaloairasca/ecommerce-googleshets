308a129fb065989afc23bb14ece6b98bae39cfd4
Gonzalo Airasca <gonzaloairasca@gmail.com>
2023-02-24 21:11:24 -0300

    con checkout a wsp

--- components/CartItemList/CartItemList.tsx
+++ components/CartItemList/CartItemList.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Link from "next/link";
 import { Item, Button, Loader, Message } from "semantic-ui-react";
 import { CartItemType } from "@/store/Cart";
+import Product from "@/pages/[product]";
 
 type CartItemListProps = {
   items: CartItemType[];
@@ -35,46 +36,22 @@ const CartItemList = ({
         </Link>
       </div>
     );
-
-  const mapCartItemsToItems = (items: CartItemType[]) =>
-    items.map((cartItem) => {
-      const { id, title, quantity, price, image } = cartItem;
-
-      return (
-        <>
-          <div>
-            <img src={image} alt={title} />
-          </div>
-        </>
-      );
-
-      // return {
-      //   childKey: id,
-      //   header: (
-      //     <Link href={`/${title}`} passHref>
-      //       <Item.Header as="a">{title}</Item.Header>
-      //     </Link>
-      //   ),
-      //   image: (
-      //     <Item.Image
-      //       src={image}
-      //       alt={title}
-      //       size="small"
-      //       style={{ background: "#f2f2f2" }}
-      //     />
-      //   ),
-      //   meta: `${quantity} x ${price}`,
-      //   description: "Some more information goes here....",
-      //   extra: (
-      //     <Button
-      //       basic
-      //       icon="remove"
-      //       floated="right"
-      //       onClick={() => removeFromCart(cartItem)}
-      //     />
-      //   ),
-      // };
-    });
+  const text = items
+    .reduce(
+      (message, product) =>
+        message.concat(
+          `*${product.title.toUpperCase()} -  Cantidad ${product.quantity} - $${
+            product.price
+          } \n`
+        ),
+      ``
+    )
+    .concat(
+      `\nTotal $${items.reduce(
+        (total, product) => total + product.quantity * product.price,
+        0
+      )}`
+    );
 
   return (
     <div className="mt-20 min-h-screen">
@@ -103,8 +80,9 @@ const CartItemList = ({
       ))}
       <div className="w-full flex justify-center mt-8">
         <Link
-          href="https://api.whatsapp.com/send?phone=346….302&text=Hola%20,te%20asesoramos%20por
-%20whatsapp%20gestiona%20tu%20compra%20por%20este%20canal"
+          href={`https://api.whatsapp.com/send?phone=5491158621648&text=${encodeURIComponent(
+            "Elegí estos productos en tu tienda online \n \n"
+          )}${encodeURIComponent(text)}`}
           target="_blank"
         >
           <button className="bg-black py-3 px-4 text-lg text-white rounded-lg">
